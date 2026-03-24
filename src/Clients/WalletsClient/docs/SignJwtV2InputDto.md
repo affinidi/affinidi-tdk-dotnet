94ec7ac1c3d4d52b387cd09240653b8aabaaa3f6
Affinidi Pte. Ltd. <pub.dev@affinidi.com>
2026-03-24 23:32:05 +0000

    feat: update all clients

---
+++ src/Clients/WalletsClient/docs/SignJwtV2InputDto.md
@@ -0,0 +1,12 @@
+# AffinidiTdk.WalletsClient.Model.SignJwtV2InputDto
+DTO contains payload of JWT to be signed
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**Payload** | **Object** |  | 
+**KeyId** | **string** | wallet key ID to use for signing (defaults to wallet&#39;s default key) | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
--- src/Clients/WalletsClient/src/AffinidiTdk.WalletsClient/Model/WalletV2Dto.cs
+++ src/Clients/WalletsClient/src/AffinidiTdk.WalletsClient/Model/WalletV2Dto.cs
@@ -44,9 +44,10 @@ namespace AffinidiTdk.WalletsClient.Model
         /// <param name="ari">ARI of the wallet.</param>
         /// <param name="algorithm">algorithm used to generate key for the wallet.</param>
         /// <param name="keys">keys.</param>
+        /// <param name="services">list of service endpoints associated with this wallet.</param>
         /// <param name="createdAt">createdAt.</param>
         /// <param name="modifiedAt">modifiedAt.</param>
-        public WalletV2Dto(string id = default, string did = default, string name = default, string description = default, Object didDocument = default, string ari = default, string algorithm = default, List<WalletDtoKeysInner> keys = default, string createdAt = default, string modifiedAt = default)
+        public WalletV2Dto(string id = default, string did = default, string name = default, string description = default, Object didDocument = default, string ari = default, string algorithm = default, List<WalletDtoKeysInner> keys = default, List<ServiceEndpointDto> services = default, string createdAt = default, string modifiedAt = default)
         {
             this.Id = id;
             this.Did = did;
@@ -56,6 +57,7 @@ namespace AffinidiTdk.WalletsClient.Model
             this.Ari = ari;
             this.Algorithm = algorithm;
             this.Keys = keys;
+            this.Services = services;
             this.CreatedAt = createdAt;
             this.ModifiedAt = modifiedAt;
         }
@@ -116,6 +118,13 @@ namespace AffinidiTdk.WalletsClient.Model
         public List<WalletDtoKeysInner> Keys { get; set; }
 
         /// <summary>
+        /// list of service endpoints associated with this wallet
+        /// </summary>
+        /// <value>list of service endpoints associated with this wallet</value>
+        [DataMember(Name = "services", EmitDefaultValue = false)]
+        public List<ServiceEndpointDto> Services { get; set; }
+
+        /// <summary>
         /// Gets or Sets CreatedAt
         /// </summary>
         [DataMember(Name = "createdAt", EmitDefaultValue = false)]
@@ -143,6 +152,7 @@ namespace AffinidiTdk.WalletsClient.Model
             sb.Append("  Ari: ").Append(Ari).Append("\n");
             sb.Append("  Algorithm: ").Append(Algorithm).Append("\n");
             sb.Append("  Keys: ").Append(Keys).Append("\n");
+            sb.Append("  Services: ").Append(Services).Append("\n");
             sb.Append("  CreatedAt: ").Append(CreatedAt).Append("\n");
             sb.Append("  ModifiedAt: ").Append(ModifiedAt).Append("\n");
             sb.Append("}\n");
